f3e884bde40b34643dd83e44be874aeb7a124b0b
Ion Villarreal <ivillarreal@agetic.gob.bo>
2022-04-16 11:40:56 -0400

    fix: actualizadas versiones estables de dependencias, eliminadas dependencias sin utilizar

--- components/ui/form/FormInputDate.tsx
+++ components/ui/form/FormInputDate.tsx
@@ -1,7 +1,7 @@
-import AdapterDateFns from '@mui/lab/AdapterDayjs'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { Controller } from 'react-hook-form'
-import DatePicker from '@mui/lab/DatePicker'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { FormHelperText, TextField, Typography } from '@mui/material'
 import { RegisterOptions } from 'react-hook-form/dist/types/validator'
 import { Control } from 'react-hook-form/dist/types/form'
@@ -38,7 +38,7 @@ export const FormInputDate = ({
           field: { onChange, value, ref },
           fieldState: { error },
         }) => (
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
               onChange={onChange}
               value={value}
