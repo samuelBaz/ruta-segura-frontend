74ad39c7d6158ed7f08e57696340f2052c36bf3b
Ion Villarreal <ivillarreal@agetic.gob.bo>
2022-07-31 08:10:48 -0400

    chore: actualización de dependencias, corrección en proporción de columnas

--- pages/admin/home.tsx
+++ pages/admin/home.tsx
@@ -74,19 +74,7 @@ const Home: NextPage = () => {
                       container
                       direction="row"
                       spacing={{ xs: 2, md: 3 }}
-                      columns={
-                        rolUsuario.modulos.some(
-                          (modulo) => modulo.subModulo.length == 1
-                          // Si algun módulo tiene solo un sub modulo, las columnas en tamaño md o xl serán más pequeñas
-                        )
-                          ? {
-                              xs: 4,
-                              sm: 8,
-                              md: 8,
-                              xl: 8,
-                            }
-                          : { xs: 2, sm: 8, md: 12, xl: 12 }
-                      }
+                      columns={{ xs: 2, sm: 8, md: 12, xl: 12 }}
                     >
                       {modulo.subModulo.map((subModulo, index2) => (
                         <Grid
