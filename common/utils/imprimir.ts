0fe07e804d14e752478c74f43e9757ab11da4aaf
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-04-01 23:10:58 -0400

    fix: corregido error en función de impresión ocasionado por no encontrar valor en regex

--- common/utils/imprimir.ts
+++ common/utils/imprimir.ts
@@ -1,25 +1,32 @@
 import { Constantes } from '../../config'
 
 const obtenerNombreFuncionAnterior = (d: number) => {
-  const error = new Error()
-  let r = ''
-  if (error.stack != null) {
-    const firefoxMatch = (error.stack.split('\n')[d].match(/^.*(?=@)/) || [])[0]
-    const chromeMatch = (
-      (((error.stack.split('at ') || [])[1 + d] || '').match(
-        /(^|\.| <| )(.*[^(<])( \()/
-      ) || [])[2] || ''
-    )
-      .split('.')
-      .pop()
-    const safariMatch = error.stack.split('\n')[d]
+  try {
+    const error = new Error()
+    let r = ''
+    if (error.stack != null) {
+      const firefoxMatch = (error.stack.split('\n')[d].match(/^.*(?=@)/) ||
+        [])[0]
+      const chromeMatch = (
+        (((error.stack.split('at ') || [])[1 + d] || '').match(
+          /(^|\.| <| )(.*[^(<])( \()/
+        ) || [])[2] || ''
+      )
+        .split('.')
+        .pop()
+      const safariMatch = error.stack.split('\n')[d]
 
-    // firefoxMatch ? console.log('firefoxMatch', firefoxMatch) : void 0;
-    // chromeMatch ? console.log('chromeMatch', chromeMatch) : void 0;
-    // safariMatch ? console.log('safariMatch', safariMatch) : void 0;
-    r = firefoxMatch || chromeMatch || safariMatch
+      // firefoxMatch ? console.log('firefoxMatch', firefoxMatch) : void 0;
+      // chromeMatch ? console.log('chromeMatch', chromeMatch) : void 0;
+      // safariMatch ? console.log('safariMatch', safariMatch) : void 0;
+      r = firefoxMatch || chromeMatch || safariMatch
+    }
+    return r
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(`🚨`, e)
+    return ''
   }
-  return r
 }
 
 export const imprimir = (...mensaje: any[]) => {
