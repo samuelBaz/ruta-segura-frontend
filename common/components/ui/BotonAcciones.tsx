73ba6f459600d2de4dc26df77dcbc5fadfaae5b6
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-05-18 10:28:38 -0400

    fix: corregido evento de hover de botones personalizados que evitaba que desaparezcan los mensajes

--- common/components/ui/BotonAcciones.tsx
+++ common/components/ui/BotonAcciones.tsx
@@ -1,4 +1,11 @@
-import { Box, IconButton, Menu, MenuItem, Typography } from '@mui/material'
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from '@mui/material'
 import { Icono } from './Icono'
 import React, { MouseEventHandler, ReactNode, useState } from 'react'
 
@@ -50,62 +57,85 @@ export const BotonAcciones = ({
   label,
   id,
 }: BotonAccionesParams) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [botonAccionesAnchorEl, setBotonAccionesAnchorEl] =
+    useState<null | HTMLElement>(null)
 
   const desplegarMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
+    setBotonAccionesAnchorEl(event.currentTarget)
   }
 
   const cerrarMenu = () => {
-    setAnchorEl(null)
+    setBotonAccionesAnchorEl(null)
+  }
+
+  const [openTooltip, setOpenTooltip] = useState(false)
+
+  const handleTooltipClose = () => {
+    setOpenTooltip(false)
+  }
+
+  const handleTooltipOpen = () => {
+    setOpenTooltip(true)
   }
 
   return (
-    <>
-      <IconButton
-        id={id}
-        aria-label={label}
-        size="small"
-        onClick={desplegarMenu}
-        color="primary"
-        style={{ textTransform: 'none' }}
-      >
-        <Icono color={desactivado ? 'disabled' : color}>{icono}</Icono>
-      </IconButton>
-      <Menu
-        id="menu-acciones"
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        open={Boolean(anchorEl)}
-        onClose={cerrarMenu}
-        autoFocus={false}
-      >
-        {acciones
-          .filter((value) => value.mostrar)
-          .map((accion, index) => (
-            <MenuItem
-              sx={{ p: 2 }}
-              id={accion.id}
-              key={`${index}-accion`}
-              onClick={(event) => {
-                cerrarMenu()
-                if (accion.accion) return accion.accion(event)
-              }}
-              disabled={accion.desactivado}
-            >
-              <Icono color={accion.color}>{accion.icono}</Icono>
-              <Box width={'20px'} />
-              <Typography variant={'body2'}>{accion.titulo}</Typography>
-            </MenuItem>
-          ))}
-      </Menu>
-    </>
+    <Tooltip
+      title={label}
+      onClose={handleTooltipClose}
+      open={openTooltip}
+      onMouseOver={() => {
+        if (!botonAccionesAnchorEl) handleTooltipOpen()
+      }}
+    >
+      <span>
+        <IconButton
+          id={id}
+          aria-label={label}
+          size="small"
+          onClick={(event) => {
+            handleTooltipClose()
+            desplegarMenu(event)
+          }}
+          color="primary"
+          style={{ textTransform: 'none' }}
+        >
+          <Icono color={desactivado ? 'disabled' : color}>{icono}</Icono>
+        </IconButton>
+        <Menu
+          id="menu-acciones"
+          anchorEl={botonAccionesAnchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          open={Boolean(botonAccionesAnchorEl)}
+          onClose={cerrarMenu}
+          autoFocus={false}
+        >
+          {acciones
+            .filter((value) => value.mostrar)
+            .map((accion, index) => (
+              <MenuItem
+                sx={{ p: 2 }}
+                id={accion.id}
+                key={`${index}-accion`}
+                onClick={(event) => {
+                  cerrarMenu()
+                  if (accion.accion) return accion.accion(event)
+                }}
+                disabled={accion.desactivado}
+              >
+                <Icono color={accion.color}>{accion.icono}</Icono>
+                <Box width={'20px'} />
+                <Typography variant={'body2'}>{accion.titulo}</Typography>
+              </MenuItem>
+            ))}
+        </Menu>
+      </span>
+    </Tooltip>
   )
 }
--- common/components/ui/BotonOrdenar.tsx
+++ common/components/ui/BotonOrdenar.tsx
@@ -4,6 +4,7 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  Tooltip,
   Typography,
 } from '@mui/material'
 import { Icono } from './Icono'
@@ -39,84 +40,109 @@ export const BotonOrdenar = ({
   label,
   id,
 }: BotonOrdenarParams) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [botonOrdenarAnchorEl, setBotonOrdenarAnchorEl] =
+    useState<null | HTMLElement>(null)
 
   const desplegarMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
+    setBotonOrdenarAnchorEl(event.currentTarget)
   }
 
   const cerrarMenu = () => {
-    setAnchorEl(null)
+    setBotonOrdenarAnchorEl(null)
+  }
+
+  const [openTooltip, setOpenTooltip] = useState(false)
+
+  const handleTooltipClose = () => {
+    setOpenTooltip(false)
+  }
+
+  const handleTooltipOpen = () => {
+    setOpenTooltip(true)
   }
 
   return (
-    <>
-      <IconButton
-        id={id}
-        aria-label={label}
-        size="small"
-        onClick={desplegarMenu}
-        color="primary"
-        style={{ textTransform: 'none' }}
-      >
-        <Badge
-          color="secondary"
-          variant="dot"
-          badgeContent={
-            criterios.filter((value) => value.ordenar && value.orden).length
-          }
+    <Tooltip
+      title={label}
+      onClose={handleTooltipClose}
+      open={openTooltip}
+      onMouseOver={() => {
+        if (!botonOrdenarAnchorEl) handleTooltipOpen()
+      }}
+    >
+      <span>
+        <IconButton
+          id={id}
+          aria-label={label}
+          size="small"
+          onClick={(event) => {
+            handleTooltipClose()
+            desplegarMenu(event)
+          }}
+          color="primary"
+          style={{ textTransform: 'none' }}
         >
-          <Icono color={desactivado ? 'disabled' : color}>{icono}</Icono>
-        </Badge>
-      </IconButton>
-      <Menu
-        id="menu-acciones"
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        open={Boolean(anchorEl)}
-        onClose={cerrarMenu}
-        autoFocus={false}
-      >
-        {criterios.map((accion, index) => {
-          return accion.ordenar ? (
-            <MenuItem
-              sx={{ p: 2 }}
-              id={`${index}-id-orden`}
-              key={`${index}-id-orden`}
-              onClick={() => {
-                cerrarMenu()
-                const nuevosCriterios = [...criterios] // crea una copia del array original
+          <Badge
+            color="secondary"
+            variant="dot"
+            badgeContent={
+              criterios.filter((value) => value.ordenar && value.orden).length
+            }
+          >
+            <Icono color={desactivado ? 'disabled' : color}>{icono}</Icono>
+          </Badge>
+        </IconButton>
+        <Menu
+          id="menu-acciones"
+          anchorEl={botonOrdenarAnchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          open={Boolean(botonOrdenarAnchorEl)}
+          onClose={cerrarMenu}
+          autoFocus={false}
+        >
+          {criterios.map((accion, index) => {
+            return accion.ordenar ? (
+              <MenuItem
+                sx={{ p: 2 }}
+                id={`${index}-id-orden`}
+                key={`${index}-id-orden`}
+                onClick={() => {
+                  cerrarMenu()
+                  const nuevosCriterios = [...criterios] // crea una copia del array original
 
-                cambioCriterios(
-                  nuevosCriterios.map((value, indice) => ({
-                    ...value,
-                    ...{
-                      orden:
-                        index == indice ? ToggleOrden(value.orden) : undefined,
-                    },
-                  }))
-                )
-              }}
-            >
-              {accion.orden && (
-                <Icono color={'primary'} fontSize={'small'}>
-                  {accion.orden == 'asc' ? 'north' : 'south'}
-                </Icono>
-              )}
-              {!accion.orden && <Box width={'25px'} />}
-              <Box width={'20px'} />
-              <Typography variant={'body2'}>{accion.nombre}</Typography>
-            </MenuItem>
-          ) : undefined
-        })}
-      </Menu>
-    </>
+                  cambioCriterios(
+                    nuevosCriterios.map((value, indice) => ({
+                      ...value,
+                      ...{
+                        orden:
+                          index == indice
+                            ? ToggleOrden(value.orden)
+                            : undefined,
+                      },
+                    }))
+                  )
+                }}
+              >
+                {accion.orden && (
+                  <Icono color={'primary'} fontSize={'small'}>
+                    {accion.orden == 'asc' ? 'north' : 'south'}
+                  </Icono>
+                )}
+                {!accion.orden && <Box width={'25px'} />}
+                <Box width={'20px'} />
+                <Typography variant={'body2'}>{accion.nombre}</Typography>
+              </MenuItem>
+            ) : undefined
+          })}
+        </Menu>
+      </span>
+    </Tooltip>
   )
 }
