b84003dc5893b11a0466c1294e2805c67bf8a982
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-07-13 17:48:22 -0400

    chore: corrección de formato de bitácora, con items de estado y fechas dinámicas

--- common/components/ui/Bitacora.tsx
+++ common/components/ui/Bitacora.tsx
@@ -1,189 +1,135 @@
-import { Card, CardContent, CardHeader, Grid, Paper, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import {
   Timeline,
-  TimelineItem,
-  TimelineSeparator,
-  TimelineDot,
-  TimelineContent,
-  timelineItemClasses,
   TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  timelineItemClasses,
+  TimelineSeparator,
 } from '@mui/lab'
 import { ColorIconoType, IBitacoraAcciones, IBitacoraItems } from '../../types'
-import { ReactNode } from 'react'
-import dayjs from 'dayjs'
 import { Icono } from './Icono'
 
-type TamanioIconoType = 'small' | 'medium' | 'large' | 'inherit'
+type IconoSizeType = 'small' | 'medium' | 'large' | 'inherit'
 
 export interface IBitacoraProps {
   titulo: string
   acciones: Array<IBitacoraAcciones>
   onClick?: (item: IBitacoraItems) => void
-  children?: ReactNode
-  maxHeight?: number
-  tamanioIcono?: TamanioIconoType
-  mostrarHoras?: boolean
+  iconSize?: IconoSizeType
 }
 
-export const Bitacora = (
-  {
-    titulo = '',
-    acciones = [],
-    onClick,
-    children,
-    maxHeight,
-    tamanioIcono,
-    mostrarHoras = false,
-  }: IBitacoraProps) => {
-
+export const Bitacora = ({
+  titulo = '',
+  acciones = [],
+  onClick,
+  iconSize,
+}: IBitacoraProps) => {
   return (
     <>
       {acciones.length > 0 ? (
         <>
-          <Paper
-            sx={{
-              borderRadius: 3,
-              p: 1,
-              overflowY: 'auto',
-              ...(maxHeight ? { maxHeight: maxHeight } : {})
-            }}>
-            <Grid
-              container
-              direction={'row'}
-              justifyContent={'space-between'}
+          <Grid container direction={'row'} justifyContent={'space-between'}>
+            <Typography textAlign={'center'} pb={1} px={1}>
+              {titulo}
+            </Typography>
+          </Grid>
+          {acciones.map((accion, index) => (
+            <Box
+              key={`id-accion-item-${index}`}
+              sx={{
+                px: 1,
+                py: 0,
+                ...(index === 0
+                  ? { borderRadius: '12px 12px 0px 0px' }
+                  : index === acciones.length - 1
+                  ? { borderRadius: '0px 0px 12px 12px' }
+                  : {}),
+              }}
             >
               <Typography
-                textAlign={'center'}
-                pb={1}
-                px={1}
+                color="text.secondary"
+                variant={'subtitle2'}
+                sx={{ pb: 1, pt: 1 }}
               >
-                {titulo}
+                {accion.titulo}
               </Typography>
-            </Grid>
-            {acciones.map((accion, index) => (
-              <Card
-                key={`id-accion-item-${index}`}
-                variant="elevation"
+              <Timeline
                 sx={{
-                  px: 1,
+                  [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                  },
+                  m: 0,
                   py: 0,
-                  ...(index === 0
-                    ? { borderRadius: '12px 12px 0px 0px' }
-                    : index === acciones.length - 1
-                      ? { borderRadius: '0px 0px 12px 12px' }
-                      : {}
-                  )
+                  pr: 0,
                 }}
               >
-                <CardHeader
-                  subheader={accion.titulo}
-                  sx={{ pb: 0, pt: 0.5 }}
-                />
-                <CardContent sx={{
-                  p: 0,
-                  my: 0,
-                  "&:last-child": {
-                    paddingBottom: 0
-                  }
-                }}>
-                  <Timeline
-                    sx={{
-                      [`& .${timelineItemClasses.root}:before`]: {
-                        flex: 0,
-                        padding: 0,
-                      },
-                      m: 0,
-                      py: 0,
-                      pr: 0,
-                    }}
-                  >
-                    {accion.items.map((item, index) => (
-                      <TimelineItem key={`accion-${index}`} sx={{ py: 0 }}>
-                        <TimelineSeparator>
-                          <TimelineDot
-                            color={item.color_icono as ColorIconoType}
-                            sx={{
-                              mt: 1,
-                              boxShadow: 'none'
-                            }}
-                            variant={item.icono
-                              ? 'filled'
-                              : 'filled'
-                            }
+                {accion.items.map((item, index) => (
+                  <TimelineItem key={`accion-${index}`} sx={{ py: 0 }}>
+                    <TimelineSeparator>
+                      <TimelineDot
+                        color={item.color_icono as ColorIconoType}
+                        sx={{
+                          boxShadow: 'none',
+                        }}
+                        variant={item.icono ? 'filled' : 'filled'}
+                      >
+                        {item.icono && (
+                          <Icono
+                            color={'inherit'}
+                            fontSize={iconSize ? iconSize : 'small'}
                           >
-                            {item.icono && (
-                              <Icono
-                                color={'inherit'}
-                                fontSize={tamanioIcono
-                                  ? tamanioIcono
-                                  : 'small'
-                                }
-                              >
-                                {item.icono}
-                              </Icono>
-                            )}
-                          </TimelineDot>
-                          {index < accion.items.length - 1 && <TimelineConnector />}
-                        </TimelineSeparator>
-                        <TimelineContent
-                          sx={{ py: 0 }}
-                          onClick={() => {
-                            if (onClick) {
-                              onClick(item)
-                            }
-                          }}>
-                          {item.accion && (
-                            <Typography
-                              variant="subtitle2"
-                              fontWeight={'bold'}
-                              display={'inline'}
-                              pr={1}
-                            >
-                              {item.accion}
-                            </Typography>
-                          )}
-                          <Typography
-                            variant="subtitle2"
-                            display={'inline'}>
-                            {item.descripcion}
-                          </Typography>
-                          <Typography
-                            color="GrayText"
-                            variant="subtitle2">
-                            {dayjs(item.fecha).format(mostrarHoras ? 'DD/MM/YYYY HH:mm' : 'DD/MM/YYYY') !== 'Invalid Date'
-                              ? dayjs(item.fecha).format(mostrarHoras ? 'DD/MM/YYYY HH:mm' : 'DD/MM/YYYY')
-                              : item.fecha
-                            }
-                          </Typography>
-                          {children}
-                        </TimelineContent>
-                      </TimelineItem>
-                    ))}
-                  </Timeline>
-                </CardContent>
-              </Card>
-            ))}
-          </Paper>
+                            {item.icono}
+                          </Icono>
+                        )}
+                      </TimelineDot>
+                      {index < accion.items.length - 1 && <TimelineConnector />}
+                    </TimelineSeparator>
+                    <TimelineContent
+                      sx={{ py: 1 }}
+                      onClick={() => {
+                        if (onClick) {
+                          onClick(item)
+                        }
+                      }}
+                    >
+                      {item.titulo && (
+                        <Typography variant="subtitle2" fontWeight={'medium'}>
+                          {`${item.titulo}`}
+                        </Typography>
+                      )}
+                      <Typography
+                        variant="subtitle2"
+                        display={'inline'}
+                        fontWeight={'regular'}
+                      >
+                        {item.descripcion}
+                      </Typography>
+                      <Typography
+                        color="text.secondary"
+                        variant="subtitle2"
+                        fontWeight={'light'}
+                      >
+                        {item.fecha}
+                      </Typography>
+                      {item.componente}
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </Timeline>
+            </Box>
+          ))}
         </>
       ) : (
-        <Paper
-          elevation={1}
-          sx={{
-            minHeight: 100,
-            borderRadius: 3,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
+        <Typography
+          textAlign={'center'}
+          variant="subtitle1"
+          color={'text.secondary'}
         >
-          <Typography
-            textAlign={'center'}
-            variant="subtitle1"
-            color={'text.secondary'}
-          >
-            No realizaste ninguna acción
-          </Typography>
-        </Paper>
+          No realizaste ninguna acción
+        </Typography>
       )}
     </>
   )
--- common/types/bitacoraTypes.ts
+++ common/types/bitacoraTypes.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react'
+
 export interface IBitacoraAcciones {
   titulo: string
   items: Array<IBitacoraItems>
@@ -6,9 +8,10 @@ export interface IBitacoraAcciones {
 export interface IBitacoraItems {
   color_icono: ColorIconoType
   descripcion: string
-  accion?: string
+  titulo?: string
   fecha: string
   icono?: string
+  componente?: ReactNode
 }
 
 export type ColorIconoType = 'success' | 'error' | 'info' | 'warning'
