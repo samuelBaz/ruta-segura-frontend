6db80e9d5f3e8d73ef251b3b285e05da1ac685db
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-07-31 15:01:03 -0400

    chore: corrección de code smells en test de creación y desbloqueo de cuenta

--- test/e2e/playwright/usuarios.spec.ts
+++ test/e2e/playwright/usuarios.spec.ts
@@ -3,21 +3,21 @@ import { expect, test } from '@playwright/test'
 test('Usuarios - crear/editar usuario', async ({ page, isMobile }) => {
   const fs = require('fs')
 
-  const rawCiudadanos = fs.readFileSync(process.env.PATH_CIUDADANOS)
+  const usuariosSegipPrueba: Array<SegipPruebaType> = JSON.parse(
+    fs.readFileSync(process.env.PATH_SEGIP)
+  )
 
-  const ciudadanos: Array<usuarioPruebaType> = JSON.parse(rawCiudadanos)
+  const indice = Math.floor(Math.random() * usuariosSegipPrueba.length)
 
-  const indice = Math.floor(Math.random() * ciudadanos.length)
-
-  if (ciudadanos.length == 0) {
+  if (usuariosSegipPrueba.length == 0) {
     return
   }
 
-  const algunCiudadano = ciudadanos.filter(
+  const algunUsuario = usuariosSegipPrueba.filter(
     (value) => value.Observacion == null
   )[indice]
 
-  if (!algunCiudadano) {
+  if (!algunUsuario) {
     return
   }
 
@@ -30,33 +30,31 @@ test('Usuarios - crear/editar usuario', async ({ page, isMobile }) => {
   await page.getByRole('button', { name: 'Usuarios', exact: true }).click()
   await page.locator('#agregarUsuario').click()
   await page.locator('#nroDocumento').click()
-  await page.locator('#nroDocumento').fill(algunCiudadano.NumeroDocumento)
-  await page.locator('#nombre').fill(algunCiudadano.Nombres)
-  await page
-    .locator('#primerApellido')
-    .fill(algunCiudadano.PrimerApellido ?? '')
+  await page.locator('#nroDocumento').fill(algunUsuario.NumeroDocumento)
+  await page.locator('#nombre').fill(algunUsuario.Nombres)
+  await page.locator('#primerApellido').fill(algunUsuario.PrimerApellido ?? '')
   await page
     .locator('#segundoApellido')
-    .fill(algunCiudadano.SegundoApellido ?? '')
-  await page.locator('#fechaNacimiento').fill(algunCiudadano.FechaNacimiento)
+    .fill(algunUsuario.SegundoApellido ?? '')
+  await page.locator('#fechaNacimiento').fill(algunUsuario.FechaNacimiento)
   await page.locator('#roles').first().click()
   await page.getByRole('option', { name: 'Usuario' }).click()
   await page.getByRole('option', { name: 'Usuario' }).press('Escape')
   await page
     .locator('#correoElectronico')
-    .fill(`agepic-${algunCiudadano.NumeroDocumento}@yopmail.com`)
+    .fill(`agepic-${algunUsuario.NumeroDocumento}@yopmail.com`)
   await page.getByRole('button', { name: 'Guardar' }).click()
 
   await page.waitForResponse((response) => response.url().includes('/usuarios'))
 
   await page.locator('#accionFiltrarUsuarioToggle').click()
-  await page.locator('#nombre').fill(algunCiudadano.NumeroDocumento)
+  await page.locator('#nombre').fill(algunUsuario.NumeroDocumento)
   expect(
-    page.getByRole('cell', { name: algunCiudadano.NumeroDocumento })
+    page.getByRole('cell', { name: algunUsuario.NumeroDocumento })
   ).toBeDefined()
 })
 
-interface usuarioPruebaType {
+interface SegipPruebaType {
   ComplementoVisible: number
   NumeroDocumento: string
   Complemento: string
