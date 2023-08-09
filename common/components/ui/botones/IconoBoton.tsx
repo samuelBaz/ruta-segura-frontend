b5b3e45e6acc40f352deba097aa4c672e3ef2e73
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-08-09 09:32:53 -0400

    fix: añadida opción de variantes de botones en componente IconoBoton

--- common/components/ui/botones/IconoBoton.tsx
+++ common/components/ui/botones/IconoBoton.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@mui/material'
 import { IconoTooltip } from './IconoTooltip'
+import { OverridableStringUnion } from '@mui/types'
+import { ButtonPropsVariantOverrides } from '@mui/material/Button/Button'
 
 interface IconoBotonParams {
   id: string
   variante?: 'icono' | 'boton'
+  variant?: OverridableStringUnion<
+    'text' | 'outlined' | 'contained',
+    ButtonPropsVariantOverrides
+  >
   texto: string
   icono: string
   descripcion: string
@@ -15,13 +21,14 @@ export const IconoBoton = ({
   texto,
   icono,
   variante = 'boton',
+  variant = 'contained',
   descripcion,
   accion,
 }: IconoBotonParams) => {
   return variante == 'boton' ? (
     <Button
       id={id}
-      variant={'contained'}
+      variant={variant}
       sx={{ ml: 1, mr: 1 }}
       size={'small'}
       onClick={() => {
