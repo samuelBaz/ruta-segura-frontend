95b726c64f8b27fc827299163b556add90d538b4
Jorge Torrez <jor.g.t97@gmail.com>
2024-02-29 17:46:43 -0400

    feat: se añadió ejemplo de formulario formato 'Wizard' (paso a paso) en Storybook

---
+++ stories/components/plantillas/StepsFormContext/components/stepContainer/Form1.tsx
@@ -0,0 +1,91 @@
+import { forwardRef, useImperativeHandle } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Box } from '@mui/material'
+import { useFormState } from '../FormContext'
+import { FormInputText } from '../../../../../../common/components/ui/form'
+
+export interface Form1Ref {
+  validar: () => Promise<void>
+}
+
+interface PropsComponente {
+  accionSiguiente: () => Promise<void>
+}
+
+interface Form1Type {
+  nombre: string
+  primerApellido: string
+  segundoApellido: string
+  nroDocumento: string
+}
+
+const Form1 = (props: PropsComponente, ref: React.Ref<unknown | undefined>) => {
+  const { setFormData, formData } = useFormState()
+  const { control, handleSubmit } = useForm<Form1Type>({
+    defaultValues: formData,
+  })
+
+  const onSubmit: SubmitHandler<Form1Type> = (data) => {
+    setFormData((prevFormData) => ({ ...prevFormData, ...data }))
+    props.accionSiguiente()
+  }
+
+  useImperativeHandle(
+    ref,
+    (): Form1Ref => ({
+      validar: () => {
+        return handleSubmit(onSubmit)()
+      },
+    })
+  )
+
+  return (
+    <Box display={'flex'} flexDirection={'column'} gap={2}>
+      <FormInputText
+        id={'nombre'}
+        control={control}
+        name="nombre"
+        label="Nombre"
+        type={'text'}
+        rules={{
+          required: 'Este campo es requerido',
+        }}
+      />
+
+      <FormInputText
+        id={'primerApellido'}
+        control={control}
+        name="primerApellido"
+        label="Primer Apellido"
+        type={'text'}
+        rules={{
+          required: 'Este campo es requerido',
+        }}
+      />
+
+      <FormInputText
+        id={'segundoApellido'}
+        control={control}
+        name="segundoApellido"
+        label="Segundo Apellido"
+        type={'text'}
+        rules={{
+          required: 'Este campo es requerido',
+        }}
+      />
+
+      <FormInputText
+        id={'nroDocumento'}
+        control={control}
+        name="nroDocumento"
+        label="Nro. de Documento C.I."
+        type={'text'}
+        rules={{
+          required: 'Este campo es requerido',
+        }}
+      />
+    </Box>
+  )
+}
+
+export default forwardRef(Form1)
