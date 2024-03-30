5407cac305b8bb48e64ecbbc296b08edf16f91c3
Ion Villarreal <ivillarreal@agetic.gob.bo>
2024-03-30 09:59:54 -0400

    feat: se añadio versión y nombre de servicio y entorno para API de estado

--- pages/api/estado.ts
+++ pages/api/estado.ts
@@ -1,20 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { nombreRama, commitID, commitDate } from '../../common/utils'
-
-type Data = {
-  estado: string
-  hora: number
-  b: any
-  cid: any
-  cd: any
-}
+import {
+  commitDate,
+  commitID,
+  nombreRama,
+  serviceName,
+  versionNumber,
+} from '../../common/utils'
+import { Constantes } from '../../config'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   res.status(200).json({
+    servicio: serviceName(),
+    version: versionNumber(),
+    entorno: Constantes.appEnv,
     estado: `Servicio funcionando correctamente 🙌`,
     hora: new Date().getTime(),
     b: await nombreRama(),
